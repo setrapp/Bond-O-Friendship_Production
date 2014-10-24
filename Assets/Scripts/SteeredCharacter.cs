ec162a634cce3b3d3311e5cc1b73ce825d72d519
Sam Trapp <set9069@rit.edu>
2014-10-24 16:14:49 -0400

    Added seek and flee behaviors and a basic NPC.

---
+++ Assets/Scripts/SteeredCharacter.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SteeredCharacter : MonoBehaviour {
+	public Vector3 target;
+	public bool seeking;
+	public SteeringBehaviors steering;
+	
+	void Start()
+	{
+		if (steering == null)
+		{
+			steering = GetComponent<SteeringBehaviors>();
+		}
+	}
+
+	void Update()
+	{
+		if (steering != null)
+		{
+			if (seeking)
+			{
+				steering.Seek(target);
+			}
+			else
+			{
+				steering.Flee(target);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/SteeringBehaviors.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SteeringBehaviors : MonoBehaviour {
+	public SimpleMover mover;
+
+	void Start()
+	{
+		if (mover == null)
+		{
+			mover = GetComponent<SimpleMover>();
+		}
+	}
+
+
+	public void Seek(Vector3 seekTarget)
+	{
+		mover.Accelerate(seekTarget - transform.position);
+	}
+
+	public void Flee(Vector3 fleeTarget)
+	{
+		mover.Accelerate(transform.position- fleeTarget);
+	}
+
+}
