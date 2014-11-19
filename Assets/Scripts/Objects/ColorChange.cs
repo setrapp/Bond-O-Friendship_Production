bd9b616ce2c4b07eac584f10b242d6a32f2f0724
Bryan O'Neill <bro9918@g.rit.edu>
2014-11-18 19:21:06 -0500

    Fixed LevelTest colliders

---
+++ Assets/Scripts/Objects/ColorChange.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorChange : MonoBehaviour {
+
+	private Color startColor;
+	private Color endColor;
+	private float coolDown = 1.0f;
+	private bool changing = false;
+
+	// Use this for initialization
+	void Start () {
+		startColor = gameObject.GetComponent<Renderer>().material.color;
+		endColor = new Color(0.9f, 0.9f, 0.9f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(changing == true)
+			gameObject.GetComponent<Renderer>().material.color = Color.Lerp(startColor, endColor, 2.0f*Time.deltaTime);
+		else if(gameObject.GetComponent<Renderer>().material.color != startColor)
+			gameObject.GetComponent<Renderer>().material.color = Color.Lerp(endColor, startColor, 2.0f*Time.deltaTime);
+
+	}
+
+	void OnCollisionEnter(Collision col){
+		if(col.transform.tag == "Converser" || col.transform.tag == "Pulse")
+			changing = true;
+	}
+
+	void OnCollisionExit(Collision col){
+		if(col.transform.tag == "Converser" || col.transform.tag == "Pulse")
+		{
+			changing = false;
+		}
+	}
+}
