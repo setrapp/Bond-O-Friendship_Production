f170c1c7748766e7aebe51a837dda15c68fad60c
Arun Abraham <aa6096@g.rit.edu>
2015-02-20 17:17:59 -0500

    Adding Fireflies to generative scene

---
+++ Assets/SwarmHandler.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SwarmHandler : MonoBehaviour {
+
+    public int fireFlyCount = 12;
+    public GameObject fireFlyPrefab;
+    public List<GameObject> fireFlyList;
+    public GameObject fireFly;
+
+	// Use this for initialization
+	void Start () {
+
+	    for(int i=0; i<fireFlyCount;i++)
+        {
+           fireFly = (GameObject)Instantiate(fireFlyPrefab, transform.position, Random.rotation);
+           fireFlyList.Add(fireFly);
+           fireFly.transform.parent = transform;
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        for (int i = 0; i < fireFlyCount; i++)
+        {
+           
+        }
+	}
+}
