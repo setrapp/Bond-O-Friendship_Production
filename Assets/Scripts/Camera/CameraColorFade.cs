fbb4cf29392dc3332ec70fd6702073f9432fa9f6
Bryan O'Neill <bro9918@g.rit.edu>
2015-02-20 20:42:29 -0500

    Added simple particle systems to guide players in the ether, should be replaced after GDC. Also added camera fade script that activates with a boolean.

---
+++ Assets/Scripts/Camera/CameraColorFade.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraColorFade : MonoBehaviour {
+
+	private Color startingColor;
+	private Color currentColor;
+	public Color fadeColor;
+	public float fadeSpeed;
+	public bool fading;
+
+	// Use this for initialization
+	void Start () {
+		fading = true;
+		startingColor = Camera.main.backgroundColor;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		currentColor = Camera.main.backgroundColor;
+		if(fading == true)
+			Camera.main.backgroundColor = Color.Lerp(currentColor, fadeColor, Time.deltaTime*fadeSpeed);
+		else
+			Camera.main.backgroundColor = Color.Lerp(currentColor, startingColor, Time.deltaTime*fadeSpeed);
+	}
+}
