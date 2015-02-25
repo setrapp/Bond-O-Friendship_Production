2baf8ceb00f31e9069703abddb9d820249d9a37b
Sam Trapp <set9069@rit.edu>
2015-02-24 21:12:53 -0500

    Fixes to Intimacy.

---
+++ Assets/Scripts/Fluff/FluffPopper.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FluffPopper : MonoBehaviour {
+
+	private void AttachFluff(Fluff fluff)
+	{
+		if (fluff != null)
+		{
+			fluff.PopFluff();
+		}
+	}
+}
