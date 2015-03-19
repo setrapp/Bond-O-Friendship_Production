0782c4057038c054469af9fdf6016246a40a5435
Sam Trapp <set9069@rit.edu>
2015-03-19 19:19:56 -0400

    Made Cluster node puzzle more generic so we can use it more.

--- Assets/Scripts/Tutorial/RotateDoor.cs
+++ Assets/Scripts/Tutorial/RotateDoor.cs
@@ -3,28 +3,39 @@ using System.Collections;
 
 public class RotateDoor : MonoBehaviour {
 
-	private NodePuzzle nodePuzzle;
+	public float stopAngle;
+	public float speed = 8;
 	private bool rotating;
+	public float rotateDireciton = 1;
 
-	// Use this for initialization
-	void Start () {
-		nodePuzzle = transform.parent.GetChild(0).GetComponent<NodePuzzle>();
+	void Start()
+	{
+		if (transform.localRotation.eulerAngles.z > stopAngle % 360)
+		{
+			rotateDireciton = -1;
+		}
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		if(nodePuzzle != null)
-		{
-			if(nodePuzzle.solved == true)
-				rotating = true;
-		}
-
 		if(rotating == true)
 		{
-			if(name == "Gate Hinge" && transform.localRotation.eulerAngles.z < 237.0f)
-				transform.Rotate(Vector3.forward * Time.deltaTime * 8.0f);
+			if ((rotateDireciton > 0 && transform.localRotation.eulerAngles.z < stopAngle) || (rotateDireciton < 0 && transform.localRotation.eulerAngles.z > stopAngle))
+			{
+				transform.Rotate(Vector3.forward * Time.deltaTime * speed *rotateDireciton);
+			}
+			/*if(name == "Gate Hinge" && transform.localRotation.eulerAngles.z < 237.0f)
+				transform.Rotate(Vector3.forward * Time.deltaTime * speed);
 			if(name == "Gate Hinge 2" && transform.localRotation.eulerAngles.z > 56.0f)
-				transform.Rotate(-Vector3.forward * Time.deltaTime * 8.0f);
+				transform.Rotate(-Vector3.forward * Time.deltaTime * speed);*/
+		}
+	}
+
+	public void ClusterNodesSolved(ClusterNodePuzzle puzzle)
+	{
+		if (puzzle != null && puzzle.solved)
+		{
+			rotating = true;
 		}
 	}
 }
