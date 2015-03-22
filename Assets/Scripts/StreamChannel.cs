9def3f7b669600f93fdd73361102246712f6ab4f
Sam Trapp <trapp.s.e@gmail.com>
2015-03-22 17:03:20 -0400

    StreamDrawer creates channels for a stream to pass through, using a Tracer as a guide. The drawer creates a channel new channel when enough of a turn is made between tracer vertices and logs channels into a series. Drawn objects should be copied before stopping the game and then pasted into the editor for later use.

---
+++ Assets/Scripts/StreamChannel.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StreamChannel : MonoBehaviour {
+
+	public GameObject bed;
+	public GameObject bank1;
+	public GameObject bank2;
+
+
+}
---
+++ Assets/Scripts/StreamChannelSeries.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class StreamChannelSeries : MonoBehaviour {
+	[SerializeField]
+	public List<StreamChannel> channels;
+	[SerializeField]
+	public List<StreamChannelChange> streamChanges;
+	
+}
+
+[System.Serializable]
+public class StreamChannelChange
+{
+	public StreamChannel preChangeChannel;
+	public StreamChannelSeries nextSeries;
+}
