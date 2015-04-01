a158c966cd9af8125498c048300f9afe8058a559
Sam Trapp <set9069@rit.edu>
2015-04-01 15:43:26 -0400

    Added camera zoom functionality  that can be triggered by WaitPad progress (as long as the pad keeps track of its portion complete).

--- Assets/Scripts/Objects/ZoomCamera.cs
+++ Assets/Scripts/Objects/ZoomCamera.cs
@@ -8,8 +8,15 @@ public class ZoomCamera : MonoBehaviour {
 	private Camera splitCamera;
 	private float startZoom;
 	public float zoomTarget;
-	public float reachAtPortion = 1;
+	public float startZoomPortion = 0;
+	public float endZoomPortion = 1;
 	private float oldPortionComplete;
+	[Header("Resetting Controls")]
+	private bool resetting = false;
+	public float resetDelay = 0.5f;
+	public float resetAcceleration = 1;
+	public float resetMaxSpeed = 5;
+	public float resetSpeed = 0f;
 
 	void Start()
 	{
@@ -23,19 +30,61 @@ public class ZoomCamera : MonoBehaviour {
 
 	void Update()
 	{
-		if (reachAtPortion <= 0)
+		if (endZoomPortion <= 0)
 		{
 			return;
 		}
 
-		if (oldPortionComplete != triggerPad.portionComplete)
+		if (triggerPad.activated && !resetting && oldPortionComplete > 0)
 		{
-			float alterPortionComplete = triggerPad.portionComplete / reachAtPortion;
-			float zoom = (startZoom * ) + (zoomTarget * triggerPad.portionComplete);
+			StartCoroutine(ResetToStart());
+		}
+
+		bool updateZoom = resetting;
+
+
+		if (!triggerPad.activated && oldPortionComplete != triggerPad.portionComplete)
+		{
+			oldPortionComplete = triggerPad.portionComplete;
+			updateZoom = true;
+		}
+
+		if (updateZoom)
+		{
+			float alterPortionComplete = Mathf.Clamp01((oldPortionComplete - startZoomPortion) / (endZoomPortion - startZoomPortion));
+			float zoom = (startZoom * (1 - alterPortionComplete)) + (zoomTarget * alterPortionComplete);
 			mainCamera.orthographicSize = zoom;
 			splitCamera.orthographicSize = zoom;
-
-			oldPortionComplete = triggerPad.portionComplete;
 		}
+	}
+
+	public IEnumerator ResetToStart()
+	{
+		resetting = true;
+		yield return new WaitForSeconds(resetDelay);
+		oldPortionComplete = endZoomPortion;
+
+		while (oldPortionComplete > 0)
+		{
+			resetSpeed += resetAcceleration * Time.deltaTime;
+			if (resetSpeed > resetMaxSpeed)
+			{
+				resetSpeed = resetMaxSpeed;
+			}
+
+			float backZoom = resetSpeed * Time.deltaTime;
+			if (oldPortionComplete - backZoom < 0)
+			{
+				oldPortionComplete = 0;
+			}
+			else
+			{
+				oldPortionComplete -= backZoom;
+				
+			}
+
+			yield return null;
+		}
+		resetting = false;
 	}
 }
