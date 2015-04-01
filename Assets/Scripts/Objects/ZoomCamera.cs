28d4bb2e6b84ee8fe46f68bcf03f157d1131beba
Sam Trapp <set9069@rit.edu>
2015-04-01 14:23:23 -0400

    Added ability to zoom a camera.

---
+++ Assets/Scripts/Objects/ZoomCamera.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZoomCamera : MonoBehaviour {
+
+	public WaitPad triggerPad;
+	private Camera mainCamera;
+	private Camera splitCamera;
+	private float startZoom;
+	public float zoomTarget;
+	public float reachAtPortion = 1;
+	private float oldPortionComplete;
+
+	void Start()
+	{
+		if (CameraSplitter.Instance != null)
+		{
+			mainCamera = CameraSplitter.Instance.splitCamera1;
+			splitCamera = CameraSplitter.Instance.splitCamera2;
+			startZoom = mainCamera.orthographicSize;
+		}
+	}
+
+	void Update()
+	{
+		if (reachAtPortion <= 0)
+		{
+			return;
+		}
+
+		if (oldPortionComplete != triggerPad.portionComplete)
+		{
+			float alterPortionComplete = triggerPad.portionComplete / reachAtPortion;
+			float zoom = (startZoom * ) + (zoomTarget * triggerPad.portionComplete);
+			mainCamera.orthographicSize = zoom;
+			splitCamera.orthographicSize = zoom;
+
+			oldPortionComplete = triggerPad.portionComplete;
+		}
+	}
+}
