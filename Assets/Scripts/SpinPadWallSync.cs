05315e4b856c2bef334eac4c03715c3bf8b2c922
unknown <set9069@SND2536-02.main.ad.rit.edu>
2015-04-03 23:55:52 -0400

    Got two membranes rotating in sync, forcing them both to move as fast as the slower of the two. But seriously though, it looks pretty cool.

---
+++ Assets/Scripts/SpinPadWallSync.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpinPadWallSync : MonoBehaviour {
+
+	public GameObject wallEnd1;
+	public GameObject wallEnd2;
+	//public GameObject centerBlock;
+	public float spinRadius = 5.5f;
+	private Vector3 center;
+	private Vector3 oldWallEndPos1;
+	private Vector3 oldWallEndPos2;
+
+	void Start()
+	{
+		center = (wallEnd1.transform.position + wallEnd2.transform.position) / 2;
+	}
+
+	void Update()
+	{
+		/*float blockRadius = centerBlock.transform.localScale.x / 2;
+		float endRadius1 = wallEnd1.transform.localScale.x / 2;
+		float endRadius2 = wallEnd2.transform.localScale.x / 2;*/
+
+		Vector3 toWallEnd1 = wallEnd1.transform.position - center;
+		toWallEnd1.z = 0;
+		toWallEnd1 = toWallEnd1.normalized * (spinRadius);
+
+		Vector3 toWallEnd2 = wallEnd2.transform.position - center;
+		toWallEnd2.z = 0;
+		toWallEnd2 = toWallEnd2.normalized * (spinRadius);
+
+
+
+		Vector3 newWallEndPos1 = center + toWallEnd1;
+		Vector3 newWallEndPos2 = center + toWallEnd2;
+
+		if ((newWallEndPos2 - oldWallEndPos2).sqrMagnitude < (newWallEndPos1 - oldWallEndPos1).sqrMagnitude)
+		{
+			newWallEndPos1 = center - toWallEnd2;
+		}
+		else
+		{
+			newWallEndPos2 = center - toWallEnd1;
+		}
+
+		wallEnd1.transform.position = new Vector3(newWallEndPos1.x, newWallEndPos1.y, wallEnd1.transform.position.z);
+		wallEnd2.transform.position = new Vector3(newWallEndPos2.x, newWallEndPos2.y, wallEnd2.transform.position.z);
+
+		oldWallEndPos1 = newWallEndPos1;
+		oldWallEndPos2 = newWallEndPos2;
+	}
+}
