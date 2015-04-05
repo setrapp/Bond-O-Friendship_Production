00d5a770fe0c20f7f596586c02f9a25e803dfa1e
Arun Abraham <smart.abraham@gmail.com>
2015-04-05 04:29:32 -0400

    starting custom mesh for tails.

---
+++ Assets/CreateTailMesh.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateTailMesh : MonoBehaviour {
+
+    //assuming 60 fps, 3 seconds
+    public int maxSize = 180;
+
+    public Vector3[] vertices;
+    public int[] indices;
+    public Vector3[] normals;
+    public Vector2[] uvs;
+
+
+	// Use this for initialization
+	void Start () {
+        MeshFilter meshFilter = GetComponent<MeshFilter>();
+        Mesh mesh = new Mesh();
+        meshFilter.mesh = mesh;
+
+        //Vertices
+        vertices = new Vector3[maxSize*2];
+
+        //Triangles
+        indices = new int[(maxSize-1)*2];
+
+        //Normals
+        normals = new Vector3[(maxSize-1)*2];
+
+        //UVs
+
+        //UpdateMesh();
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+
+
+	}
+}
