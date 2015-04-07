18b2a4c6636b2590d084a1093f3223c6f9570255
Sam Trapp <set9069@rit.edu>
2015-04-07 17:57:37 -0400

    First pass of stream 'bringing objects to life' and allowing players to push them, instead of the stream pushing objects itself.

---
+++ Assets/Scripts/StreamFillReaction.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class StreamFillReaction : StreamReaction {
+
+	public float fillProgress = 0;
+	public float fillRate = 1;
+
+	[SerializeField]
+	public List<Renderer> fillTargetRenderers;
+	public Color unfilledTint = Color.grey;
+	public Color filledTint = Color.white;
+	private List<Color> baseColors;
+
+	void Start()
+	{
+		baseColors = new List<Color>();
+		for (int i = 0; i < fillTargetRenderers.Count; i++)
+		{
+			baseColors.Add(fillTargetRenderers[i].material.color);
+		}
+			
+		ApplyTint();
+	}
+
+	public override void React(Stream stream)
+	{
+		if (fillProgress < 1)
+		{
+			fillProgress = Mathf.Clamp01(fillProgress + fillRate * Time.deltaTime);
+
+			if (fillProgress >= 1)
+			{
+				if (reactionCollider != null)
+				{
+					reactionCollider.enabled = false;
+				}
+				if (reactionBody != null)
+				{
+					reactionBody.isKinematic = false;
+				}
+			}
+
+			ApplyTint();
+		}
+	}
+
+	private void ApplyTint()
+	{
+		Color tint = (unfilledTint * (1 - fillProgress)) + (filledTint * fillProgress);
+		for (int i = 0; i < fillTargetRenderers.Count && i < baseColors.Count; i++)
+		{
+			fillTargetRenderers[i].material.color = baseColors[i] * tint;
+		}
+	}
+}
---
+++ Assets/Scripts/StreamReaction.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class StreamReaction : MonoBehaviour {
+
+	public Collider reactionCollider;
+	public Rigidbody reactionBody;
+
+	public abstract void React(Stream stream);
+}
