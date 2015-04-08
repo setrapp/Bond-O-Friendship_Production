e72784212906de806a43221b4504d57539527980
Sam Trapp <set9069@rit.edu>
2015-04-08 12:28:30 -0400

    Added StreamScalingReaction to imitate things ballooning as they fill up.

---
+++ Assets/Scripts/StreamScalingReaction.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class StreamScalingReaction : StreamReaction {
+
+	[SerializeField]
+	public List<StreamScalingStats> scalees;
+
+	void Start()
+	{
+		for (int i = 0; i < scalees.Count; i++)
+		{
+			scalees[i].scalee.localScale = scalees[i].unscaled;
+		}
+	}
+
+	public override void React(float actionRate)
+	{
+		if (reactionProgress < 1)
+		{
+			base.React(actionRate);
+
+			for (int i = 0; i < scalees.Count; i++)
+			{
+				scalees[i].scalee.localScale = (scalees[i].unscaled * (1 - reactionProgress)) + (scalees[i].scaled * reactionProgress);
+			}
+		}
+	}
+}
+
+[System.Serializable]
+public class StreamScalingStats
+{
+	public Transform scalee;
+	public Vector3 unscaled;
+	public Vector3 scaled;
+}
