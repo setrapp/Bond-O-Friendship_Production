62422d92d1457aab5879a5fe001c34e583874d4f
Arun Abraham <smart.abraham@gmail.com>
2015-04-08 12:33:09 -0400

    Working on tail

--- Assets/CreateTailMesh.cs
+++ Assets/CreateTailMesh.cs
@@ -4,12 +4,17 @@ using System.Collections;
 public class CreateTailMesh : MonoBehaviour {
 
     //assuming 60 fps, 3 seconds
-    public int maxSize = 180;
+    public int maxLength = 180;
 
+
+ 
     public Vector3[] vertices;
     public int[] indices;
     public Vector3[] normals;
     public Vector2[] uvs;
+
+    public Vector3 right;
+    private int currentIndex = -1;
 
 
 	// Use this for initialization
@@ -17,15 +22,16 @@ public class CreateTailMesh : MonoBehaviour {
         MeshFilter meshFilter = GetComponent<MeshFilter>();
         Mesh mesh = new Mesh();
         meshFilter.mesh = mesh;
+        right = new Vector3(1, 0, 0);
 
         //Vertices
-        vertices = new Vector3[maxSize*2];
+        vertices = new Vector3[maxLength * 2];
 
         //Triangles
-        indices = new int[(maxSize-1)*2];
+        indices = new int[(maxLength - 1) * 2];
 
         //Normals
-        normals = new Vector3[(maxSize-1)*2];
+        normals = new Vector3[(maxLength - 1) * 2];
 
         //UVs
 
@@ -35,6 +41,9 @@ public class CreateTailMesh : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
+
+        currentIndex++;
+ 
 
 
 
---
+++ Assets/TracePath.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TracePath : MonoBehaviour {
+
+    public int maxLength = 180;
+    public float startWidth = 1.0f;
+    public float endWidth = 1.0f;
+    
+    private int currentIndex = -1;
+
+    public Vector3[] recentPath;
+
+    //Test via line renderer
+    LineRenderer lineRenderer;
+
+
+	// Use this for initialization
+	void Start () {
+
+        recentPath = new Vector3[maxLength];
+        recentPath[0] = transform.position;
+        currentIndex++;
+        lineRenderer = GetComponent<LineRenderer>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Remove last vertex from the tail every update
+        if (currentIndex > 0) 
+            RemovePointFromEnd(ref recentPath, ref currentIndex);
+
+        //If the object has moved, add the new point to the array
+        if (transform.position != recentPath[0] && currentIndex >= 0)
+        {
+            MoveOneIndexForward(ref recentPath, ref currentIndex);
+            AddPointAtBeginning(ref recentPath, transform.position);
+        }
+
+        lineRenderer.SetVertexCount(currentIndex);
+        for (int i = 0; i < currentIndex; i++)
+            lineRenderer.SetPosition(i, recentPath[i]);
+
+	}
+
+    void AddPointAtBeginning(ref Vector3[] vectorArray, Vector3 point)
+    {
+        recentPath[0] = transform.position;
+    }
+
+    void RemovePointFromEnd(ref Vector3[] vectorArray, ref int indicesFilled)
+    {
+        //destroy previously existing vector
+        //vectorArray[indicesFilled] = null;
+        indicesFilled--;
+    }
+
+    void MoveOneIndexForward(ref Vector3[] vectorArray, ref int indicesFilled)
+    {
+        indicesFilled++;
+        for (int i = indicesFilled; i > 0; i--)
+        {
+            vectorArray[indicesFilled] = vectorArray[indicesFilled - 1];
+        }
+    }
+}
