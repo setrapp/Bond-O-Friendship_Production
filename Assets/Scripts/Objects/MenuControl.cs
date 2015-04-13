2b8f4f99b4806e2c0e247bda6964f9cc90b42765
Sam Trapp <set9069@rit.edu>
2015-04-13 17:56:28 -0400

    Added post bond streams and a placeholder transition to critical path level 1.

--- Assets/Scripts/Objects/MenuControl.cs
+++ Assets/Scripts/Objects/MenuControl.cs
@@ -40,6 +40,12 @@ public class MenuControl : MonoBehaviour {
                 startScene = message.message;
                 Destroy(message.gameObject);
             }
+			if (message != null && message.messageName == "LevelLoadInstant")
+			{
+				startScene = message.message;
+				Destroy(message.gameObject);
+				MainMenuLoadLevel();
+			}
         }
 
         if(deviceCount > 0)
