57ac3d444adc314a5c95c34ea61005e7b0808c41
Ben Snyder <bws7462@rit.edu>
2015-04-23 18:47:21 -0400

    More work on Asymmetry objects/mechanics

--- Assets/Scripts/Fluff/FluffConverter.cs
+++ Assets/Scripts/Fluff/FluffConverter.cs
@@ -6,10 +6,6 @@ public class FluffConverter : MonoBehaviour {
 
 
 	public GameObject fluffPrefab;
-	//public Material greenFluffMaterial;
-	//public float fluffSound;
-	//public float spawnRate = 3.0f;
-	//public float minimumForce = 3.0f;
 	public float maximumForce = 10.0f;
 	public float angleShot;
 
@@ -17,25 +13,25 @@ public class FluffConverter : MonoBehaviour {
     public float maxAngle = 0f;
 
     public GameObject fluffShooter;
-	//public Color myColor;
-	//public float alpha;
-	//public GameObject Point;
-
-	//public GameObject generatorTop;
-	
-
-	private int colorPicker;
 	private float velocity;
 	private Vector3 targetAngle;
-	private Vector3 baseTarget;
 
+    private GameObject orbToShrink;
+
+    public float pullDuration = 1f;
+    public float convertDuration = 2f;
+    private float t = 0f;
+    private Vector3 orbStartingSize;
+    private Vector3 orbStartingPosition;
+
+    private bool pulling = false;
+
+    //Editor Testing.
     public bool fire = false;
-
     private bool lastFire = false;
+
 	// Use this for initialization
 	void Start () {
-		//alpha = 1.0f;
-		//baseTarget = generatorTop.transform.position;
 	}
 	
     
@@ -48,6 +44,9 @@ public class FluffConverter : MonoBehaviour {
         }
 
         lastFire = fire;
+
+        if (orbToShrink != null)
+            PullInOrb();
     }
 	
 	public void ConvertToFluffs () 
@@ -63,27 +62,84 @@ public class FluffConverter : MonoBehaviour {
 
         foreach (GameObject fluff in fluffList)
         {
-            fluff.transform.parent = fluffShooter.transform;
-            fluff.transform.localPosition = Vector3.zero;
+            fluff.transform.position = fluffShooter.transform.position;
 
             Fluff fluffF = fluff.GetComponent<Fluff>();
-            float velocity = Random.Range(500F, 1000F);
-            angleShot = Random.Range(minAngle, maxAngle);
-            
+            velocity = Random.Range(500F, 1000F);
+            angleShot = Random.Range(minAngle, maxAngle);            
             targetAngle = Quaternion.Euler(0, 0, angleShot) * new Vector3(velocity, velocity, 0);
 
             targetAngle = -fluffShooter.transform.TransformDirection(targetAngle);
-            
-
             fluffF.Pass(targetAngle);
         }
 	}
+
+    public void PullInOrb()
+    {
+        if(pulling)
+        {
+            t = Mathf.Clamp(t + (Time.deltaTime / pullDuration), 0.0f, 1.0f);
+            orbToShrink.transform.position = Vector3.Lerp(orbStartingPosition, transform.position, t);
+
+            if (t == 1.0f)
+            {                
+                t = 0.0f;
+                pulling = false;
+            }
+        }
+        else
+        {
+            ShrinkOrb();
+        }
+    }
+
+    public void ShrinkOrb()
+    {
+
+        t = Mathf.Clamp(t + (Time.deltaTime / convertDuration), 0.0f, 1.0f);
+
+        orbToShrink.transform.localScale = Vector3.Lerp(orbStartingSize, Vector3.zero, t);
+
+        orbToShrink.transform.position = Vector3.Lerp(transform.position, fluffShooter.transform.position, t);
+
+        if (t == 1.0f)
+        {
+            GameObject.Destroy(orbToShrink);
+            orbToShrink = null;
+            orbStartingSize = Vector3.zero;
+            t = 0.0f;
+
+            ConvertToFluffs();
+        }
+
+    }
 
     void OnTriggerEnter(Collider collide)
     {
         if (collide.name == "Blossom" || collide.name == "Blossom(Clone)")
         {
-            fire = !fire;
+            if (orbToShrink == null)
+            {
+                orbToShrink = collide.gameObject;
+                orbStartingSize = collide.transform.localScale;
+                orbStartingPosition = collide.transform.position;
+                pulling = true;
+                //orbToShrink.transform.position = transform.position;
+            }
+        }
+    }
+
+    void OnTriggerStay(Collider collide)
+    {
+        if (collide.name == "Blossom" || collide.name == "Blossom(Clone)")
+        {
+            if (orbToShrink == null)
+            {
+                orbToShrink = collide.gameObject;
+                orbStartingSize = collide.transform.localScale;
+                orbStartingPosition = collide.transform.position;
+                pulling = true;
+            }
         }
     }
 
