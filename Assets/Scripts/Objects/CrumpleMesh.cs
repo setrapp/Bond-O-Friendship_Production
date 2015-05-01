ae2991b0b88630355ecfee0db1c0f587158ef7ca
Ben Snyder <bws7462@rit.edu>
2015-04-30 20:54:44 -0400

    Wiggle wiggle wiggle wiggle wiggle yeah

---
+++ Assets/Scripts/Objects/CrumpleMesh.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrumpleMesh : MonoBehaviour
+{
+    // This script is placed in public domain. The author takes no responsibility for any possible harm.
+
+    public bool x = true;
+    public bool y = true;
+    public bool z = true;
+
+    public float scale = 1.0f;
+    public float speed = 1.0f;
+    bool recalculateNormals = false;
+
+    private Vector3[] baseVertices;
+    //private var noise : Perlin;
+
+    void Start()
+    {
+        //noise = new Perlin ();
+        scale = Random.Range(.1f, .3f);
+        speed = Random.Range(1.0f, 2.0f);
+    }
+
+    void Update()
+    {
+
+
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+
+        if (baseVertices == null)
+            baseVertices = mesh.vertices;
+
+        var vertices = new Vector3[baseVertices.Length];
+
+        float timex = Time.time * speed + 0.1365143f;
+        float timey = Time.time * speed + 1.21688f;
+        float timez = Time.time * speed + 2.5564f;
+        for (var i = 0; i < vertices.Length; i++)
+        {
+            var vertex = baseVertices[i];
+
+            if(x)
+            vertex.x += Mathf.PerlinNoise(timex + vertex.x, timex + vertex.z) * scale;
+            if(y)
+                vertex.y += Mathf.PerlinNoise(timey + vertex.x, timey + vertex.y) * scale;
+            if(z)
+            vertex.z += Mathf.PerlinNoise(timez + vertex.x, timez + vertex.z) * scale;
+
+            vertices[i] = vertex;
+        }
+
+        mesh.vertices = vertices;
+
+        if (recalculateNormals)
+            mesh.RecalculateNormals();
+        mesh.RecalculateBounds();
+    }
+
+}
