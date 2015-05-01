c83da881b768af283d5b5adeb25f80c756dd0622
Ben Snyder <bws7462@rit.edu>
2015-04-30 22:25:22 -0400

    NODES BE LIKE WOAH IM JELLY

--- Assets/Scripts/Objects/CrumpleMesh.cs
+++ Assets/Scripts/Objects/CrumpleMesh.cs
@@ -13,49 +13,76 @@ public class CrumpleMesh : MonoBehaviour
     public float speed = 1.0f;
     bool recalculateNormals = false;
 
+    public bool useOtherValues = false;
+
+    public bool wilin = true;
+    public bool wasWilin = false;
+
+    public bool firstWil = true;
+
+    public CrumpleMesh valuesToUse;
+
     private Vector3[] baseVertices;
     //private var noise : Perlin;
 
     void Start()
     {
         //noise = new Perlin ();
-        scale = Random.Range(.1f, .3f);
-        speed = Random.Range(1.0f, 2.0f);
+        if (useOtherValues && valuesToUse != null)
+        {
+            scale = valuesToUse.scale;
+            speed = valuesToUse.speed;
+        }
+        else
+        {
+            scale = Random.Range(.1f, .3f);
+            speed = Random.Range(1.0f, 2.0f);
+        }
     }
 
     void Update()
     {
-
-
-        Mesh mesh = GetComponent<MeshFilter>().mesh;
-
-        if (baseVertices == null)
-            baseVertices = mesh.vertices;
-
-        var vertices = new Vector3[baseVertices.Length];
-
-        float timex = Time.time * speed + 0.1365143f;
-        float timey = Time.time * speed + 1.21688f;
-        float timez = Time.time * speed + 2.5564f;
-        for (var i = 0; i < vertices.Length; i++)
+        if (useOtherValues && valuesToUse != null)
         {
-            var vertex = baseVertices[i];
-
-            if(x)
-            vertex.x += Mathf.PerlinNoise(timex + vertex.x, timex + vertex.z) * scale;
-            if(y)
-                vertex.y += Mathf.PerlinNoise(timey + vertex.x, timey + vertex.y) * scale;
-            if(z)
-            vertex.z += Mathf.PerlinNoise(timez + vertex.x, timez + vertex.z) * scale;
-
-            vertices[i] = vertex;
+            scale = valuesToUse.scale;
+            speed = valuesToUse.speed;
+            wilin = valuesToUse.wilin;
         }
+        if (wilin || firstWil)
+        {          
 
-        mesh.vertices = vertices;
+            Mesh mesh = GetComponent<MeshFilter>().mesh;
 
-        if (recalculateNormals)
-            mesh.RecalculateNormals();
-        mesh.RecalculateBounds();
+            if (baseVertices == null)
+                baseVertices = mesh.vertices;
+
+            var vertices = new Vector3[baseVertices.Length];
+
+            float timex = Time.time * speed + 0.1365143f;
+            float timey = Time.time * speed + 1.21688f;
+            float timez = Time.time * speed + 2.5564f;
+            for (var i = 0; i < vertices.Length; i++)
+            {
+                var vertex = baseVertices[i];
+
+                if (x)
+                    vertex.x += Mathf.PerlinNoise(timex + vertex.x, timex + vertex.z) * scale;
+                if (y)
+                    vertex.y += Mathf.PerlinNoise(timey + vertex.x, timey + vertex.y) * scale;
+                if (z)
+                    vertex.z += Mathf.PerlinNoise(timez + vertex.x, timez + vertex.z) * scale;
+
+                vertices[i] = vertex;
+            }
+
+            mesh.vertices = vertices;
+
+            if (recalculateNormals)
+                mesh.RecalculateNormals();
+            mesh.RecalculateBounds();
+
+            firstWil = false;
+        }
     }
 
 }
