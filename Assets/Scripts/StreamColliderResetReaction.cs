25c44244ee3d556eac44a9084141035bb8318e81
Sam Trapp <set9069@rit.edu>
2015-05-05 11:18:06 -0400

    Attempting to make pushables less prone to getting stuck. Unity is a liar though, so there's that.

---
+++ Assets/Scripts/StreamColliderResetReaction.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StreamColliderResetReaction : StreamReaction {
+
+	public Collider resetteeCollider;
+	private bool colliderEnabled;
+	private bool resetting = false;
+
+	void Update()
+	{
+		if (resetting)
+		{
+			//resetteeCollider.enabled = colliderEnabled;
+			//resetting = false;
+		}
+	}
+
+	public override bool React(float actionRate)
+	{
+		bool reacted = base.React(actionRate);
+		if (reacted)
+		{
+			if (actionRate >= 0 && reactionProgress >= 1)
+			{
+				colliderEnabled = resetteeCollider.enabled;
+				resetteeCollider.enabled = !colliderEnabled;
+				resetting = true;
+			}
+		}
+		return reacted;
+	}
+}
