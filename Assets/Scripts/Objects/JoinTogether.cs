0ec8d8e239613246b1734e9357ce7673fc7d5dfb
Sam Trapp <set9069@rit.edu>
2015-05-07 23:12:25 -0400

    Working on JoinTogether puzzle. Basic interaction is there.

---
+++ Assets/Scripts/Objects/JoinTogether.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JoinTogether : MonoBehaviour {
+
+	public Rigidbody baseBody = null;
+	public JoinTogetherPair moveable;
+	public JoinTogetherPair sepatationTarget;
+	public JoinTogetherPair joinTarget;
+	private Vector3 oldMoveablePosition;
+	public bool atJoin = false;
+	private float progressThreshold = 0.95f;
+
+	void Awake()
+	{
+		if (moveable.baseObject == null)
+		{
+			moveable.baseObject = gameObject;
+		}
+		if (baseBody == null)
+		{
+			baseBody = moveable.baseObject.GetComponent<Rigidbody>();
+		}
+	}
+
+	void Update()
+	{
+		if (moveable.baseObject.transform.position != oldMoveablePosition)
+		{
+			atJoin = false;
+
+			Vector3 separationToMoveable = moveable.baseObject.transform.position - sepatationTarget.baseObject.transform.position;
+			Vector3 separationToJoin = joinTarget.baseObject.transform.position - sepatationTarget.baseObject.transform.position;
+
+			separationToMoveable = Helper.ProjectVector(separationToJoin, separationToMoveable);
+			float progress = separationToMoveable.magnitude / separationToJoin.magnitude;
+
+			if (progress > 1)
+			{
+				if (baseBody != null)
+				{
+					baseBody.MovePosition(joinTarget.baseObject.transform.position);
+				}
+				else
+				{
+					moveable.baseObject.transform.position = joinTarget.baseObject.transform.position;
+				}
+				progress = 1;
+			}
+
+			if (progress >= progressThreshold)
+			{
+				atJoin = true;
+			}
+
+			moveable.pairedObject.transform.position = sepatationTarget.pairedObject.transform.position + ((joinTarget.pairedObject.transform.position - sepatationTarget.pairedObject.transform.position) * progress);
+
+			oldMoveablePosition = moveable.baseObject.transform.position;
+		}
+	}
+}
+
+[System.Serializable]
+public class JoinTogetherPair
+{
+	public GameObject baseObject;
+	public GameObject pairedObject;
+}
+
---
+++ Assets/Scripts/Objects/JoinTogetherGroup.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class JoinTogetherGroup : MonoBehaviour {
+
+	public bool allChildJoins = true;
+	[SerializeField]
+	public List<JoinTogether> joins;
+	public JoinTogetherPair groupJoinTarget;
+
+	void Start()
+	{
+		if (allChildJoins)
+		{
+			JoinTogether[] childJoins = GetComponentsInChildren<JoinTogether>();
+			for (int i = 0; i < childJoins.Length; i++)
+			{
+				if (!joins.Contains(childJoins[i]))
+				{
+					joins.Add(childJoins[i]);
+				}
+			}
+		}
+
+		for (int i = 0; i < joins.Count; i++)
+		{
+			if (joins[i].joinTarget.baseObject == null)
+			{
+				joins[i].joinTarget.baseObject = groupJoinTarget.baseObject;
+			}
+			if (joins[i].joinTarget.pairedObject == null)
+			{
+				joins[i].joinTarget.pairedObject = groupJoinTarget.pairedObject;
+			}
+
+			// Attach spring joints to join pieces to prevent collisions.
+			for (int j = i + 1; j < joins.Count; j++)
+			{
+				if (joins[i].baseBody != null && joins[j].baseBody != null)
+				{
+					SpringJoint noCollide = joins[i].baseBody.gameObject.AddComponent<SpringJoint>();
+					noCollide.connectedBody = joins[j].baseBody;
+					noCollide.spring = 0;
+					noCollide.damper = 0;
+					noCollide.anchor = Vector3.zero;
+					noCollide.autoConfigureConnectedAnchor = false;
+					noCollide.connectedAnchor = Vector3.zero;
+				}
+				
+			}
+		}
+	}
+
+	void Update()
+	{
+		bool allJoined = true;
+		for (int i = 0; i < joins.Count && allJoined; i++)
+		{
+			if (!joins[i].atJoin)
+			{
+				allJoined = false;
+			}
+		}
+
+		if (allJoined)
+		{
+			for (int i = 0; i < joins.Count && allJoined; i++)
+			{
+				if (joins[i].baseBody != null)
+				{
+					joins[i].baseBody.isKinematic = true;
+				}
+			}
+		}
+	}
+}
