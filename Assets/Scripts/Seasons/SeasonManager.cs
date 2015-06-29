9a24acbbd313a5115a59cc1877e46378713152b7
Sam Trapp <trapp.s.e@gmail.com>
2015-06-29 16:27:41 -0400

    Started work on seasons stuff for critical path.

---
+++ Assets/Scripts/Seasons/SeasonManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SeasonManager : MonoBehaviour {
+	// NOTE: This script should appear on the level object with a tag set to Island.
+
+
+	public ActiveSeason activeSeason = ActiveSeason.DRY;
+
+	public enum ActiveSeason
+	{
+		DRY = 1,
+		WET = 2,
+		COLD = 4
+	}
+}
---
+++ Assets/Scripts/Seasons/SeasonObjectReaction.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SeasonObjectReaction : SeasonReaction {
+
+	protected override void Start()
+	{
+		base.Start();
+	}
+
+	void Update()
+	{
+		if (manager == null)
+		{
+			return;
+		}
+
+		if (season != manager.activeSeason)
+		{
+			season = manager.activeSeason;
+		}
+	}
+}
