a578e6fd2183f1427612dcb9187410c41a203dd5
Robert Lumsden <lummieb@gmail.com>
2015-07-14 22:36:43 -0400

    intimacy Development

--- Assets/Scripts/Stream Arise.cs
+++ Assets/Scripts/Stream Arise.cs
@@ -3,11 +3,34 @@ using System.Collections;
 
 public class StreamArise : MonoBehaviour {
 
+	public GameObject myThreadPuzzle;
+	private Vector3 myPos;
+	public float startZ;
+	public float endZ;
+	private float myZ;
+
+
 	// Use this for initialization
 	void Start () {
-	
+
+		endZ = transform.position.z;
+		myZ = startZ;
+
 	}
-	
+
+	void FixedUpdate () {
+
+		myPos = transform.position;
+		My
+		transform.position = myPos;
+
+		if (myThreadPuzzle.GetComponent<ThreadParent> ().solved) 
+		{
+
+		}
+
+	}
+
 	// Update is called once per frame
 	void Update () {
 	
