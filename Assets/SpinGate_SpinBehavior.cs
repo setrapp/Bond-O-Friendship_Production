91b8c4ab9c3c2a615f211e1031575630c983ac9c
Arun Abraham <aa6096@g.rit.edu>
2015-07-20 17:17:43 -0400

    flat floors

---
+++ Assets/SpinGate_SpinBehavior.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpinGate_SpinBehavior : MonoBehaviour {
+
+    public bool activated = false;
+    public float rotationSpeed = -5.0f; //5 degrees/frame counter-clockwise
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (activated)
+            transform.RotateAroundLocal(Vector3.forward, Time.deltaTime * rotationSpeed);
+	}
+}
