3fb53e84a5970ecc9f6c3935b8d9d8db60c9fe25
Arun Abraham <aa6096@g.rit.edu>
2015-07-21 16:26:27 -0400

    Added cluster that mirrors another cluster. TODO: Glow when node is revealed, Dummy cluster

---
+++ Assets/ConnectMirroringNodes.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConnectMirroringNodes : MonoBehaviour {
+
+    public ClusterNodePuzzle clusterToMirror;
+
+	// Use this for initialization
+	void Awake () {
+
+        float numberOfNodes = this.gameObject.GetComponent<ClusterNodePuzzle>().nodes.Count;
+        ClusterNode node, nodeToMirror;
+
+        for (int i = 0; i < numberOfNodes; i++)
+        {
+            node = this.gameObject.GetComponent<ClusterNodePuzzle>().nodes[i];
+            nodeToMirror = clusterToMirror.nodes[i];
+            node.gameObject.GetComponent<PairedClusterNode>().mirrorNode = nodeToMirror;
+        }
+	
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+	}
+}
