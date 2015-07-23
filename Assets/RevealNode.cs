c9fa5f75ae6e4ab9112c97a24436035c79e685c9
Arun Abraham <aa6096@g.rit.edu>
2015-07-23 16:49:39 -0400

    Finished functionality for Mirroring & Dummy Cluster Puzzles

---
+++ Assets/RevealNode.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RevealNode : MonoBehaviour {
+
+    public MirroringClusterNode parentNode;
+
+	// Use this for initialization
+	void Start () {
+        parentNode = transform.parent.GetComponent<MirroringClusterNode>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetComponent<Renderer>().enabled = parentNode.revealed;    
+	}
+}
