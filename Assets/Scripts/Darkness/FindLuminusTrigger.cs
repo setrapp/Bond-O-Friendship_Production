03fc35d478976ca2df9c78bb179f6a08b5812879
Sam Trapp <trapp.s.e@gmail.com>
2015-08-05 16:52:46 -0400

    In Intimate Path, fixed stream blocker for backtracking to fluff bond and allowed lumini to be found by Globals' darkness mask.

---
+++ Assets/Scripts/Darkness/FindLuminusTrigger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FindLuminusTrigger : MonoBehaviour {
+
+	void Start()
+	{
+		if (Globals.Instance != null && Globals.Instance.darknessMask != null)
+		{
+			FindClosestLuminus luminusFinder = Globals.Instance.darknessMask.GetComponent<FindClosestLuminus>();
+			if (luminusFinder != null)
+			{
+				luminusFinder.FindAllLumini();
+			}
+		}
+	}
+}
