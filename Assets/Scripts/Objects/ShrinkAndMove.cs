8302a70b037a7f01d93bd2af2f6884d9885156f0
Sam Trapp <trapp.s.e@gmail.com>
2015-08-30 17:53:45 -0400

    Bond making nodes only move together when both players stand on them, zoom the camera as they approach, and making crumple mesh change with just one player.

--- Assets/Scripts/Objects/ShrinkAndMove.cs
+++ Assets/Scripts/Objects/ShrinkAndMove.cs
@@ -6,14 +6,17 @@ public class ShrinkAndMove : ClusterNodeColorSpecific
     public ShrinkAndMove otherPlayerNode;
     public bool isPlayer1 = false;
 
-    public bool moving = false;
+    public ShrinkMoveDirection moving = ShrinkMoveDirection.NONE;
     public bool atTarget = false;
 
     public bool fullSize = false;
 
     private GameObject player;
 
-    public Renderer child;
+    public CrumpleMesh child;
+	public float activeCrumpleSpeed;
+	public Vector3 crumpleReachOffset;
+	public float crumpleReachSpeed;
 
     private Vector3 playerPosNoZ;
     private Vector3 nodePosNoZ;
@@ -30,13 +33,20 @@ public class ShrinkAndMove : ClusterNodeColorSpecific
     private float t = 0.0f;
 
     private Color fullColor;
-    private Color transparentColor;
+    public Color transparentColor;
 
     private Vector3 childStartSize;
-    private Vector3 childEndSize;
+    public Vector3 childEndSize;
 
 	public AllowPlayerBond bondAllow;
 	public AllowPlayerBond bondDisallow;
+
+	public enum ShrinkMoveDirection
+	{
+		NONE = 0,
+		FORWARD,
+		BACKWARD
+	}
 
 	// Use this for initialization
 	override protected void Start () 
@@ -46,9 +56,7 @@ public class ShrinkAndMove : ClusterNodeColorSpecific
         endPos = target.position;
 
         fullColor = GetComponent<Renderer>().material.color;
-        transparentColor = new Color(fullColor.a, fullColor.b, fullColor.g, 0.0f);
-        childStartSize = child.transform.localScale;
-        childEndSize = Vector3.zero;
+        //transparentColor = new Color(fullColor.a, fullColor.b, fullColor.g, 0.25f);
 
         if (isPlayer1)
             player = Globals.Instance.player1.gameObject;
@@ -76,17 +84,56 @@ public class ShrinkAndMove : ClusterNodeColorSpecific
 
         playerNodeDis = Vector3.SqrMagnitude(playerPosNoZ - nodePosNoZ);
 
+		/*TODO Slow node movement when between extremes, rather than stoping completely?*/
         if (playerNodeDis < Mathf.Pow(toggleDis, 2))
         {
-            moving = true;
+            moving = ShrinkMoveDirection.FORWARD;
+			if (fullSize)
+			{
+				child.scale = activeCrumpleSpeed;
+			}
         }
-        else
-            moving = false;
+		else if (playerNodeDis < Mathf.Pow(toggleDis * 1.5f, 2))
+		{
+			moving = ShrinkMoveDirection.NONE;
+		}
+		else
+		{
+			moving = ShrinkMoveDirection.BACKWARD;
+		}
 
         if (fullSize)
         {
-            
-            t = moving ? Mathf.Clamp(t + Time.deltaTime / duration, 0.0f, 1.0f) : Mathf.Clamp(t - Time.deltaTime / moveBackDur, 0.0f, 1.0f);
+            if (moving == ShrinkMoveDirection.FORWARD)
+			{
+				// If the other node is also ready to move, move towards the other node.
+				if(otherPlayerNode == null || otherPlayerNode.moving == ShrinkMoveDirection.FORWARD)
+				{
+					t = Mathf.Clamp(t + Time.deltaTime / duration, 0.0f, 1.0f);
+				}
+
+				// Reach child crumple towards the other node.
+				child.centerOffset += crumpleReachOffset.normalized * crumpleReachSpeed * Time.deltaTime;
+				if (child.centerOffset.sqrMagnitude > crumpleReachOffset.sqrMagnitude)
+				{
+					child.centerOffset = crumpleReachOffset;
+				}
+			}
+			else if (moving == ShrinkMoveDirection.BACKWARD)
+			{
+				// Move away from the other node.
+				t = Mathf.Clamp(t - Time.deltaTime / moveBackDur, 0.0f, 1.0f);
+
+				// Return child to resting crumple center.
+				if (child.centerOffset.sqrMagnitude < Mathf.Pow(crumpleReachSpeed * Time.deltaTime, 2))
+				{
+					child.centerOffset = Vector3.zero;
+				}
+				else
+				{
+					child.centerOffset -= crumpleReachOffset.normalized * crumpleReachSpeed * Time.deltaTime;
+				}
+			}
             transform.position = Vector3.Lerp(startPos, endPos, t);
             GetComponent<Renderer>().material.color = Color.Lerp(fullColor, transparentColor, t);
             child.transform.localScale = Vector3.Lerp(childStartSize, childEndSize, t);
@@ -120,7 +167,7 @@ public class ShrinkAndMove : ClusterNodeColorSpecific
 		startPos = transform.position;
 		endPos = target.position;
 		childStartSize = child.transform.localScale;
-		childEndSize = Vector3.zero;
+		childEndSize = child.transform.localScale / 2;
 	}
 
     override protected void OnTriggerEnter(Collider collide)
