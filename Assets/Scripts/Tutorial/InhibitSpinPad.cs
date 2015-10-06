7e1154551cc78cdbe654bce89a68d3098e70fb85
Sam Trapp <trapp.s.e@gmail.com>
2015-10-05 20:02:02 -0400

    Working on making spin pad blockable again.

--- Assets/Scripts/Tutorial/InhibitSpinPad.cs
+++ Assets/Scripts/Tutorial/InhibitSpinPad.cs
@@ -6,9 +6,9 @@ public class InhibitSpinPad : MonoBehaviour {
 
 	public SpinPad spinPad;
 	[SerializeField]
-	public List<Collider> spinIgnoreCollisions;
+	public List<InhibitIgnore> spinIgnoreCollisions;
 	[SerializeField]
-	public List<Collider> backSpinIgnoreCollisions;
+	public List<InhibitIgnore> backSpinIgnoreCollisions;
 	//public bool collided;
 	public bool ignoreStream = true;
 
@@ -19,9 +19,14 @@ public class InhibitSpinPad : MonoBehaviour {
 			return;
 		}
 
-		if (!spinIgnoreCollisions.Contains(col.collider))
+		/*TODO check for spin and back spin ignore*/
+		if (!CheckIgnore(col.collider, true))
 		{
 			spinPad.spinInhibitors++;
+		}
+		if (!CheckBackIgnore(col.collider, true))
+		{
+			spinPad.spinBackInhibitors++;
 		}
 	}
 	
@@ -32,9 +37,49 @@ public class InhibitSpinPad : MonoBehaviour {
 			return;
 		}
 
-		if (!spinIgnoreCollisions.Contains(col.collider))
+		if (!CheckIgnore(col.collider, false))
 		{
-			spinPad.spinInhibitors--;
+			spinPad.spinInhibitors++;
+		}
+		if (!CheckBackIgnore(col.collider, false))
+		{
+			spinPad.spinBackInhibitors++;
 		}
 	}
+
+	private bool CheckIgnore(Collider collider, bool considerRange)
+	{
+		bool found = false;
+		for (int i = 0; i < spinIgnoreCollisions.Count; i++)
+		{
+			bool inRange = spinPad.rotationProgress >= spinIgnoreCollisions[i].ignoreMinProgress && spinPad.rotationProgress <= spinIgnoreCollisions[i].ignoreMaxProgress;
+			if (spinIgnoreCollisions[i].ignoreCollider == collider && (!considerRange || inRange))
+			{
+				found = true;
+			}
+		}
+		return found;
+	}
+
+	private bool CheckBackIgnore(Collider collider, bool considerRange)
+	{
+		bool found = false;
+		for (int i = 0; i < spinIgnoreCollisions.Count; i++)
+		{
+			bool inRange = spinPad.rotationProgress >= backSpinIgnoreCollisions[i].ignoreMinProgress && spinPad.rotationProgress <= backSpinIgnoreCollisions[i].ignoreMaxProgress;
+			if (backSpinIgnoreCollisions[i].ignoreCollider == collider && (!considerRange || inRange))
+			{
+				found = true;
+			}
+		}
+		return found;
+	}
+}
+
+[System.Serializable]
+public class InhibitIgnore
+{
+	public Collider ignoreCollider;
+	public float ignoreMinProgress = -1f;
+	public float ignoreMaxProgress = 1f;
 }
