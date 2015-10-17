25f899386b7305090381de76602bee1ed72958e2
Sam Trapp <trapp.s.e@gmail.com>
2015-10-16 20:17:04 -0400

    Showing continue menu option and continue title prompt (placeholder) in the main menu after the players have completed tutorial.

---
+++ Assets/Scripts/Globals/LevelCompleteDependentEnable.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LevelCompleteDependentEnable : MonoBehaviour {
+
+	public List<GameObject> enableTargets;
+	public bool[] requiredLevels;
+	public bool enableWhenComplete = true;
+
+	void Awake()
+	{
+		if (Globals.Instance != null)
+		{
+			bool reqsMet = enableWhenComplete;
+			for (int i = 0; i < requiredLevels.Length && i < Globals.Instance.levelsCompleted.Length; i++)
+			{
+				if (requiredLevels[i] && !Globals.Instance.levelsCompleted[i])
+				{
+					reqsMet = !enableWhenComplete;
+				}
+			}
+
+			for (int i = 0; i < enableTargets.Count; i++)
+			{
+				if (enableTargets[i] != null)
+				{
+					enableTargets[i].SetActive(reqsMet);
+				}
+			}
+		}
+	}
+
+}
