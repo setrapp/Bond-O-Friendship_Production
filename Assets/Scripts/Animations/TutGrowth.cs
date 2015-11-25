2997f736e072d83411a7f21a1edccd961992f628
Robert Lumsden <lummieb@gmail.com>
2015-11-25 12:44:35 -0500

    Growth Assets

---
+++ Assets/Scripts/Animations/TutGrowth.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutGrowth : MonoBehaviour {
+
+    // Use this for initialization
+    public Animator anim;
+    public GameObject container;
+
+	void Start () {
+        anim = GetComponent<Animator >();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (container.GetComponent<GrowthContainer>().activated == true)
+        {
+            PlayAnim();
+        }
+
+    }
+
+    void PlayAnim()
+    {
+        anim.enabled = true;
+        //anim.PlayQueued("grow", QueueMode.PlayNow);
+        //anim.PlayQueued("wobble2", QueueMode.CompleteOthers);
+    }
+}
