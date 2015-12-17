b504fc9f4a52b6f80c5d24509b9a554d15305001
Sam Trapp <trapp.s.e@gmail.com>
2015-12-17 00:24:03 -0500

    Started work on progressive bgm track, but waiting to hear about song length.

---
+++ Assets/Scripts/Globals/MusicTrackGroup.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicTrackGroup : MonoBehaviour {
+
+	public MusicTrack[] tracks;
+
+	void Start()
+	{
+		if (tracks.Length > 0)
+		{
+			tracks[0].playable = true;
+		}
+		for (int i = 0; i < tracks.Length; i++)
+		{
+			tracks[i].playable = false;
+		}
+	}
+
+	void Update()
+	{
+
+	}
+}
+
+public class MusicTrack
+{
+	public AudioSource audio;
+	public bool playable = false;
+	public float maxVolume;
+}
