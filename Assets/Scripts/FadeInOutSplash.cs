c649bd2a5e90144d542630cd5f8b5e0720227c94
Sam Trapp <trapp.s.e@gmail.com>
2016-04-07 16:51:12 -0400

    Added splash screen with MAGIC spell studios logo. Cannot remove unity splash screen without unity pro.

---
+++ Assets/Scripts/FadeInOutSplash.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FadeInOutSplash : MonoBehaviour {
+
+    public CanvasGroup fadeGroup;
+    public float fadeTime;
+    public float waitTime;
+    public string nextScene;
+    
+    void Start()
+    {
+        if (fadeGroup == null)
+        {
+            fadeGroup = GetComponent<CanvasGroup>();
+        }
+
+        if (fadeGroup != null)
+        {
+            fadeGroup.alpha = 0;
+            StartCoroutine(FadeSplash());
+        }
+    }
+
+    private IEnumerator FadeSplash()
+    {
+        // Fade in.
+        if (fadeTime > 0)
+        {
+            while (fadeGroup.alpha < 1)
+            {
+                fadeGroup.alpha += Time.deltaTime / fadeTime;
+                yield return null;
+            }
+        }
+        else
+        {
+            fadeGroup.alpha = 1;
+        }
+
+        // Wait.
+        float waitElapsed = 0;
+        while (waitElapsed < waitTime)
+        {
+            waitElapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        // Fade out.
+        if (fadeTime > 0)
+        {
+            while (fadeGroup.alpha > 0)
+            {
+                fadeGroup.alpha -= Time.deltaTime / fadeTime;
+                yield return null;
+            }
+        }
+        else
+        {
+            fadeGroup.alpha = 0;
+        }
+
+        if (nextScene != null && nextScene != "")
+        {
+            Application.LoadLevel(nextScene);
+        }
+
+        yield return null;
+    }
+}
